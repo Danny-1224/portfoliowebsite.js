e952b308fce8aaac9ca82d8697c21075ea3246b9
Danny-1224 <230806021@live.unilag.edu.ng>
2025-06-26 19:14:00 +0100

    Adding event listener

--- portfolio/practice.js
+++ portfolio/practice.js
@@ -2,3 +2,36 @@ const $menu = document.getElementById('menu');
 const $li = $menu.querySelectorAll('li');
 const $hue1 = document.querySelector('#h1');
 const $hue2 = document.querySelector('#h2');
+
+document.addEventListener("contextmenu", (event) => {
+    
+    const menuBox = $menu.getBoundingClientRect();
+    const bodyBox = { width: window.innerWidth, height: window.innerHeight }
+    const target = {  x: event.clientX, y: event.clientY }
+    const padding = { x: 30, y: 20 }
+    
+    const hitX = target.x + menuBox.width >= bodyBox.width - padding.x;
+    const hitY = target.y + menuBox.height >= bodyBox.height - padding.y;
+    
+    if ( hitX ) {
+        target.x = bodyBox.width - menuBox.width - padding.x;
+    }
+    
+    if ( hitY ) {
+        target.y = bodyBox.height - menuBox.height - padding.y;
+    }
+    
+    const $target = event.target;
+    const isMenu = $menu.contains( $target );
+    event.preventDefault();
+    
+    if( !isMenu ) {
+        $menu.style.left = target.x + 'px';
+        $menu.style.top = target.y + 'px';
+        $menu.classList.add('open');
+        $li.forEach($el => {
+          $el.classList.remove('selected');  
+        })
+    }
+    
+});
